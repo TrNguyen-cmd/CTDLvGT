6aa71124573237271ec8de9d611fb5dd57169fbe
TrNguyen-cmd <trnguyen4112006@gmail.com>
2025-10-05 14:36:21 +0700

    Buoi 3

---
+++ Câu-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Câu_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Stack<int> ST = new Stack<int>();
+            Console.Write("Nhập số nguyên cần chuyển sang hệ nhị phân: ");
+            int n = int.Parse(Console.ReadLine());
+            int d = n;
+            while(d>0)
+            {
+                ST.Push(d % 2);
+                d = d / 2;
+            }
+            Console.Write($"{n} sau khi chuyển sang hệ nhị phân: ");
+            foreach (int i in ST)
+            {
+                Console.Write(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bai1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Stack<int> stack = new Stack<int>();
+            Console.Write("Số phần tử trong stack: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("Các phần tử trong stack: ");
+            foreach (int i in stack)
+            { 
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("************************************************************************");
+            stack.Pop();
+            Console.WriteLine("Stack sau khi bỏ phần tử đầu tiên: ");
+            foreach(int i in stack)
+            { Console.Write(i+ " ");}
+            Console.WriteLine();
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("Phần tử đầu tiên của stack: "+stack.Peek());
+            Console.WriteLine("************************************************************************");
+            if (stack.Count() == 0)
+            { Console.WriteLine("Stack rỗng "); }
+            else Console.WriteLine("Stack có chứa phần tử");
+            Console.ReadKey();
+        }
+    }
+}
